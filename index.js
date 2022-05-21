582c012638ae1e12dcfae2155b40fc1a6614285c
mnz <batman@mnz.ee>
2022-05-21 13:52:32 +0300

    removed obsolete server var, can pipe the port listen isntantly
    removed adding process on exit event on every request, once should be enough

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var port = 1337;
     const browser = await puppeteer.launch({ headless: true });
 
     //Start Native Nodejs HTTP server
-    var server = http.createServer(async function (req, res) {
+    http.createServer(async function (req, res) {
         const parsedUrl = url.parse(req.url, true);
 
         if (parsedUrl.query == null) return res.end("Something went wrong, please restart.");
@@ -26,27 +26,26 @@ var port = 1337;
 
         page.goto(urlParams)
         .then(async urlresponse => {
-            console.log('urlresp', urlresponse);
             responseBody = await urlresponse.text();
             res.write(responseBody);
             res.end();
         })
         .catch(e => {
             res.end(e.message);
-        });
-
-        process.on("exit", async (code) => {
-            await browser.close();
-            console.log("Closed CloudScraper with code: " + code);
         })
-    });
         .finally(_ => {
             page.close();
         });
+    })
+    .listen(port);
 
-    server.listen(port);
     process.stdout.write('\033c');
     console.log(`   _____ _                 _ _____                     \n  / ____| |               | |  __ \\                    \n | |    | | ___  _   _  __| | |__) _ __ _____  ___   _ \n | |    | |/ _ \\| | | |/ _\` |  ___| '__/ _ \\ \\/ | | | |\n | |____| | (_) | |_| | (_| | |   | | | (_) >  <| |_| |\n  \\_____|_|\\___/ \\__,_|\\__,_|_|   |_|  \\___/_/\\_\\__,  |\n                                                  __/ |\nBy \x1b[1mPGSleepy (https://github.com/pgsleepy)\x1b[0m        |___/ \n\x1b[0m\n\x1b[7mUsage: http://localhost:${port}/?url=https://www.yoururl.com/index.php\x1b[0m`)
     console.log(`\x1b[7mMake sure to add the HTTP(S):// protocol!\x1b[0m`)
     console.log(`Proxy succesfully started on port ${port}`)
+
+    process.on("exit", async (code) => {
+        await browser.close();
+        console.log("Closed CloudScraper with code: " + code);
+    });
 })();
